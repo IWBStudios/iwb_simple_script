663e4df70d4696770def4a4fd7e37cbe16ece5a3
BigYoo <bigyookosta@gmail.com>
2024-01-14 08:29:42 +0100

    create basic function & simple config

--- client/client.lua
+++ client/client.lua
@@ -1,12 +1,20 @@
 -- CL Side
 local settings = Config.Settings
+
 function spawnPed()
 
     local pedData = Config.Ped
     
     if settings.debug then
+        print("=============DEBUG=================")
         print(pedData) -- tabela
-        print(pedData.model, pedData.pos, pedData.pospedData.pospedData.pos) -- pojedyncze dane w tabeli
+        print(pedData.name, pedData.model, pedData.pos, pedData.heading, pedData.genderName) -- pojedyncze dane w tabeli
+        print('------------')
+        for key, value in pairs(pedData) do
+            print('key:',key, 'value:',value)
+        end
+        print("===================================")
+        
     end
     -- Sposób ładowania modelu, wymagane przy tworzeniu nowych entitty 
     RequestModel(GetHashKey(pedData.model))
@@ -15,17 +23,21 @@ function spawnPed()
     end
 
     -- Ustalanie odpowiedniego typu względem danych z tabeli Config.Ped
-    if pedData.gender == 'm' then 
+    if pedData.genderName == 'm' or pedData.genderName == 'male' then 
         genderNum = 4
-    elseif pedData.gender == 'f' then
+        if settings.debug then print('if + or | Gender',pedData.genderName, 'genderNum',genderNum) end
+    elseif pedData.genderName == 'f' then
         genderNum = 5
+        if settings.debug then print('elseif f | Gender',pedData.genderName, 'genderNum',genderNum) end
+    elseif pedData.genderName == 'female' then
+        genderNum = 5
+        if settings.debug then print('elseif female | Gender',pedData.genderName, 'genderNum',genderNum) end
     else
         if settings.debug then print("błąd w cfg gamoniu :D Zapomniałeś o płci") end
     end
 
-
-    ped = CreatePed(genderNum, GetHashKey(pedData.model), pedData.pos, pedData.pos, false, true)
-    SetEntityAlpha(ped, 0, false)
+    ped = CreatePed(genderNum, GetHashKey(pedData.model), pedData.pos.x,pedData.pos.y,pedData.pos.z, pedData.heading, false, true)
+    -- SetEntityAlpha(ped, 0, false)
 
     -- if anim.scenario then
     --     TaskStartScenarioInPlace(ped, scenario, 0, true) -- begins peds animation
@@ -38,3 +50,39 @@ function spawnPed()
     -- end
 
 end
+
+spawnPed()
+
+
+
+function createBlipOnMap()
+    local blip = AddBlipForCoord(Config.BlipCoords)
+    SetBlipSprite	(blip, Config.BlipSprite)
+    SetBlipDisplay(blip, Config.BlipDisplay)
+    SetBlipScale	(blip, Config.BlipScale)
+    SetBlipColour	(blip, Config.BlipColor)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentSubstringPlayerName(Config.BlipName)
+    EndTextCommandSetBlipName(blip)
+end
+
+createBlipOnMap()
+
+local markerData = Config.Marker
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1)
+		local playerCoords = GetEntityCoords(PlayerPedId())
+		local dist = #(playerCoords - markerData.pos)
+		if dist <= 5 then
+			DrawMarker(markerData.id,markerData.pos.x,markerData.pos.y,markerData.pos.z, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 1.0, 1.0, 1.0, 145, 20, 100, 200, false, true, 2, true, nil, false)
+			if dist <= 1.5 then
+				ESX.ShowHelpNotification('~INPUT_CONTEXT~ Wciśnij E')
+				if IsControlJustPressed(0, 38)  then
+					print('Wciśnięto E')
+				end
+			end
+		end
+	end
+end)
--- config.lua
+++ config.lua
@@ -5,10 +5,27 @@ Config.Settings = {
     debug = true
 }
 
-Config.Ped = { -- Prosta tablica z informacjami o pedzie którego nam ma zrespić 
-    model = '',
-    pos = vector3(0,0,0),
-    heading = 200.0, -- Kurwa musi być `float` czyli liczba zmienno przecinkowa ;)
-    gender = '' -- Płeć 'm' albo 'f'  male/female
+Config.Ped = { -- Prosta tablica z informacjami o pedzie którego nam ma zrespić
+    name = 'josh',
+    model = 'cs_josh',
+    pos = vector3(-1298.887940, -3406.496582, 13.929688),
+    heading = 180, -- Kurwa musi być `float` czyli liczba zmienno przecinkowa ;)
+    genderName = 'male' -- Płeć 'm' albo 'f'  male/female
 }
 
+Config.PedDescription = {
+    ['johny'] = 'Deskrypcja Johnego czyli jakiś dłuższy np tekst opisujący coś.'
+}
+
+
+Config.BlipName = "Jakiś blip na mapie"
+Config.BlipCoords  = vector3(-1297.898926, -3401.103272, 13.929688)
+Config.BlipDisplay = 4
+Config.BlipSprite  = 486
+Config.BlipScale   = 0.5
+Config.BlipColor  = 0
+
+Config.Marker = {
+    id = 27,
+    pos = vector3(-1293.665894, -3403.767090, 13.929688),
+}
